7611c61ec3f9d4f1725737ebb91504dcb7b228dd
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-16 21:22:23 +0200

    Update traduction_mot_chiffre.pl

--- TP1/traduction_mot_chiffre.pl
+++ TP1/traduction_mot_chiffre.pl
@@ -25,4 +25,19 @@ fois_symb(M1, M2, R):- traduction(X,M1), traduction(Y,M2), R is X*Y.
 
 % ?- plus_symb(un, deux, R). ==> R = 3.
 % ?- fois_symb(un, deux, R). ==> R = 2.
+% ?- plus_symb(M1, M2, 4). ==>  M1 = un, M2 = trois; 
+%                               M1 = M2, M2 = deux ;
+%                               M1 = trois, M2 = un;
 
+
+plus_symb_bis(M1, M2, Max):- plus_symb(M1, M2, R), R =< Max .
+
+/* ?- plus_symb_bis(M1, M2, 4) ==>
+M1 = M2, M2 = un
+M1 = un, M2 = deux
+M1 = un, M2 = trois
+M1 = deux, M2 = un
+M1 = M2, M2 = deux
+M1 = trois, M2 = un
+
+*/
